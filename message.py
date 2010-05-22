7ba766b005ee9a656da1ffa18cd01ac347f78828
Mehul Ved <mehul.n.ved@gmail.com>
2010-05-22 12:29:19 +0530

    modified code to handle sending multiple message

--- message.py
+++ message.py
@@ -13,19 +13,22 @@ def getMsg():
 	msgOpen = open('msgFile.txt', 'r')
 	msg = msgOpen.read()
 	msg = msg.replace(" ", "+")
-#	msg = msg.replace("\n","+")
+	msg = msg.split('---')
 	return msg
 
 def postMsg(msg, groupname, token, apiVersion):
 	"""
 	Post the retrived message to SMS Gupshup
 	"""
-	postMsgUrl = "http://api.smsgupshup.com/GupshupAPI/rest?text=%s&token=%s&groupName=%s&method=groups.postMessage&v=%s" % (msg, token, groupname, apiVersion)
-	postMsgResult = sj.load(urllib.urlopen(postMsgUrl))
+	msglen = len(msg)
 
-	if "status" in postMsgResult['response']:
-		print "Your message was successfully posted."
-	else:
-		print postMsgResult['response']['error']['msg']
-		sys.exit()
+	for i in range(msglen-1):
+		postMsgUrl = "http://api.smsgupshup.com/GupshupAPI/rest?text=%s&token=%s&groupName=%s&method=groups.postMessage&v=%s" % (msg[i], token, groupname, apiVersion)
+		postMsgResult = sj.load(urllib.urlopen(postMsgUrl))
+
+		if "status" in postMsgResult['response']:
+			print "Your message was successfully posted."
+		else:
+			print postMsgResult['response']['error']['msg']
+			sys.exit()
 	
